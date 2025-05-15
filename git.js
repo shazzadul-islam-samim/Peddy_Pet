2778ebbf904acc9f1b7fcf4385a8e0f7720aa43c
shazzadul-islam-samim <Shazzadulislam090@gamil.com>
2025-05-15 08:18:37 +0600

    second commit

---
+++ git.js
@@ -0,0 +1,3 @@
+/**git remote add origin https://github.com/shazzadul-islam-samim/Peddy_Pet.git
+git branch -M main
+git push -u origin main */
--- pet.js
+++ pet.js
@@ -1,1 +1,31 @@
-console.log('pet successfully')
+console.log('pet successfully');
+
+const loadCategory=()=>{
+    fetch("https://openapi.programming-hero.com/api/peddy/categories")
+    .then(res => res.json())
+    .then(data => displayCategory(data.categories))
+    .catch(error=> console.log(error))
+}
+
+const displayCategory=(categories)=>{
+    //console.log(categories);
+    const categoryContainer= document.getElementById("category");
+
+    categories.forEach(item => {
+        console.log(item);
+        const category =item.category;
+        //console.log(category);
+        const buttonContainer=document.createElement("div");
+        buttonContainer.classList="";
+        buttonContainer.innerHTML=`
+        <button id="btn-${category}" class="btn btn-outline btn-accent px-4 py-2 w-full lg:btn-lg rounded-lg">
+        <img class="w-6 h-6" src="${item.category_icon}"> ${category}</button>
+        `
+        categoryContainer.append(buttonContainer);
+    });
+   
+}
+
+
+
+loadCategory();
